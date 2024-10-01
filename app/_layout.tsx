27173292ce96282ca2b2d8116677cb53d2cc31ea
kentoverse <kentoverse@gmail.com>
2024-09-30 23:47:25 -0400

    deploy:

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,7 +16,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    RobotoMono: require('../assets/fonts/RobotoMono-Regular.ttf'),
   });
 
   useEffect(() => {
